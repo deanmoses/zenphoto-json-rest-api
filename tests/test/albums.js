bb722ebcd4d476ac5fd4ac5dc87129a9005cfca2
Dean Moses <moses@tacocat.com>
2017-04-04 01:41:10 -0700

    Added some more of the album tests
    Transferring the tests from the Postman online service to the GitHub
    project.

--- tests/test/albums.js
+++ tests/test/albums.js
@@ -4,6 +4,11 @@
  * Test that album requests return the correct data
  */
 
+var chai = require('chai');
+var assert = chai.assert;
+var expect = chai.expect;
+var should = chai.should();
+
 var ZenSuite = require('./scripts/zen_suite.js');
 
 describe("Albums", function() {
@@ -12,11 +17,24 @@ describe("Albums", function() {
 	suite = new ZenSuite('/album1/?json')
 	suite.do('Album - shallow', function() {
 		suite.helpers.isAlbum();
+
+		it('Has at least two images', function(done) {
+			var album = this.response.body.album;
+		    album.images.should.have.length.above(1);
+		    done();
+		});
+
+		it('First subalbum should have no subalbums', function(done) {
+			var album = this.response.body.album;
+			should.not.exist(album.albums[0].albums);
+		    done();
+		});		
 	});
 
 	suite = new ZenSuite('/album1/?json=deep')
 	suite.do('Album - deep', function() {
-		// not implemented
+		suite.helpers.isAlbum();
+
 		it('First subalbum has at least two images', function(done) {
 			var album = this.response.body.album;
 		    album.albums[0].images.should.have.length.above(1);
