7b4f20243d47e630b7d1a91ea9574d6e10b3aec3
Dean Moses <moses@tacocat.com>
2017-04-04 00:53:24 -0700

    Cleanup of image tests

--- tests/test/images.js
+++ tests/test/images.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
- * Test that nonexistent albums and images return a proper 404 HTTP response
+ * Test that image requests return the correct data
  */
 
 var ZenSuite = require('./scripts/zen_suite.js');
@@ -9,23 +9,23 @@ var ZenSuite = require('./scripts/zen_suite.js');
 describe("Images", function() {
 	var suite;
 
-	suite = new ZenSuite('/album1/image1.jpg?json')
+	suite = new ZenSuite('/album1/image1.jpg/?json')
 	suite.do('Basic image', function() {
 		suite.helpers.isImage();
 	});
 
-	suite = new ZenSuite('/album1/image1.jpg?json')
+	suite = new ZenSuite('/album1/image1.jpg/?json')
 	suite.do('Image with more metadata', function() {
 
 		suite.helpers.isImage();
 
-		it('Image should have copyright', function(done) {
+		it('Has copyright', function(done) {
 			var image = this.response.body.image;
 		    image.copyright.should.equal('©Sample Copyright');
 		    done();
 		});
 
-		it('Image should have correct top level location info', function(done) {
+		it('Has correct top level location info', function(done) {
 			var image = this.response.body.image;
 		    image.city.should.equal('San Francisco');
 		    image.state.should.equal('CA');
@@ -33,13 +33,13 @@ describe("Images", function() {
 		    done();
 		});
 
-		it('Image should have correct tags', function(done) {
+		it('Has correct tags', function(done) {
 			var image = this.response.body.image;
 		    image.tags.should.be.an('array').and.include('test1');
 		    done();
 		});
 
-		it('Image should have correct EXIF metadata', function(done) {
+		it('Has correct EXIF metadata', function(done) {
 			var image = this.response.body.image;
 		    image.metadata.EXIFMake.should.equal('Sony');
 		    done();
