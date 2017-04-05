d0787cba87b3391e70fb7a05c6b59348b7feba2d
Dean Moses <moses@tacocat.com>
2017-04-05 11:25:40 -0700

    More search tests

--- tests/test/search.js
+++ tests/test/search.js
@@ -15,83 +15,88 @@ describe("Search", function() {
 	var suite;
 
 	/**
-	 * Test a search that returns images but no albums
+	 * Test search that returns images but no albums
 	 */
 	suite = new ZenSuite('/page/search/san francisco/?json')
 	suite.do('Search - image results', function() {
 		suite.helpers.isSearch();
-
-		it('Has images', function() {
-			var search = this.response.body.search;
-			should.exist(search.images);
-			search.images.should.be.an('array').and.have.length.above(4);
-			should.exist(search.images[0].url_full);
-		});
-
-		it('Has no albums', function() {
-			var search = this.response.body.search;
-			should.not.exist(search.albums);
-		});
+		hasImages(5);
+		hasNoAlbums();
 	});
 
 	/**
-	 * Test a search that returns albums but no images
+	 * Test search that returns albums but no images
 	 */
 	suite = new ZenSuite('/page/search/album1/?json')
 	suite.do('Search - album results', function() {
 		suite.helpers.isSearch();
-
-		it('Has albums', function() {
-			var search = this.response.body.search;
-			should.exist(search.albums);
-			search.albums.should.be.an('array').and.have.length.above(4);
-			should.exist(search.albums[0].path);
-		});
-
-		it('Has no images', function() {
-			var search = this.response.body.search;
-			should.not.exist(search.images);
-		});
+		hasAlbums(5);
+		hasNoImages();
 	});
 
 	/**
-	 * Test a search that returns both albums and images
+	 * Test search that returns both albums and images
 	 */
 	suite = new ZenSuite('/page/search/title/?json')
 	suite.do('Search - image and album results', function() {
 		suite.helpers.isSearch();
-
-		it('Has albums', function() {
-			var search = this.response.body.search;
-			should.exist(search.albums);
-			search.albums.should.be.an('array').and.have.length.above(0);
-			should.exist(search.albums[0].path);
-		});
-
-		it('Has images', function() {
-			var search = this.response.body.search;
-			should.exist(search.images);
-			search.images.should.be.an('array').and.have.length.above(0);
-			should.exist(search.images[0].url_full);
-		});
+		hasImages(5);
+		hasAlbums(1);
 	});	
 
 	/**
-	 * Test a search with no results
+	 * Test search that returns no results
 	 */
 	suite = new ZenSuite('/page/search/noSearchResultsHere/?json')
 	suite.do('Search - no results', function() {
 		suite.helpers.isSearch();
-
-		it('Has no images', function() {
-			var search = this.response.body.search;
-			should.not.exist(search.images);
-		});
-
-		it('Has no albums', function() {
-			var search = this.response.body.search;
-			should.not.exist(search.albums);
-		});
+		hasNoImages();
+		hasNoAlbums();
 	});
 
 });
+
+/**
+ * Assert that the search returned at least imageCount images
+ */
+function hasImages(imageCount = 1) {
+	it('Has images', function() {
+		var search = this.response.body.search;
+		should.exist(search.images);
+		search.images.should.be.an('array').and.have.length.above(imageCount - 1);
+		should.exist(search.images[0].url_full);
+	});
+};
+
+/**
+ * Assert that the search returned no images
+ */
+function hasNoImages() {
+	it('Has no images', function() {
+		var search = this.response.body.search;
+		should.not.exist(search.images);
+	});
+ }
+
+/**
+ * Assert that the search returned at least albumCount albums
+ */
+function hasAlbums(albumCount = 1) {
+	it('Has albums', function() {
+		var search = this.response.body.search;
+		should.exist(search.albums);
+		search.albums.should.be.an('array').and.have.length.above(albumCount - 1);
+		should.exist(search.albums[0].path);
+	});
+};
+
+/**
+ * Assert that the search returned no albums
+ */
+function hasNoAlbums() {
+	it('Has no albums', function() {
+		var search = this.response.body.search;
+		should.not.exist(search.albums);
+	});
+}
+
